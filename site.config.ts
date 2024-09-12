f3fee823042a0ed415d62e1e2ea61729c75cca65
xuewen.xu <xuewen.xu@meetsocial.com>
2024-09-12 21:07:38 +0800

    modify package.json

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Jack大叔的博客',
-  domain: '',
+  domain: 'blog.jack-xu.cn/',
   author: 'Jack Xu',
 
   // open graph metadata (optional)
